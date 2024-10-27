7e6fd1cf9d39cea24507bc48ed3c1f3bd407c335
rizki_amartha <rizki.putra@amartha.com>
2024-10-27 11:25:24 +0700

    change to  headless

--- features/environment.py
+++ features/environment.py
@@ -3,7 +3,7 @@ from playwright.sync_api import sync_playwright
 
 def before_scenario(context, scenario):
     context.playwright = sync_playwright().start()
-    context.browser = context.playwright.chromium.launch(headless=False)
+    context.browser = context.playwright.chromium.launch(headless=True)
     context.page = context.browser.new_page()
 
 def after_scenario(context, scenario):
